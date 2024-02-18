9ffb893fc5f87279a6e4da1329e998aaaa59f794
ianmuchyri <ianmuchiri8@gmail.com>
2024-02-18 12:53:13 +0300

    add reset button functionality to the dashboard
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

--- components/Controls.js
+++ components/Controls.js
@@ -1,41 +1,61 @@
-import { ReadyState } from 'react-use-websocket';
-import React, { useState, useCallback } from 'react';
+import { ReadyState } from "react-use-websocket";
+import React, { useState, useCallback } from "react";
 
 function Controls({ mode, sendJsonMessage, readyState }) {
-	const [status, setStatus] = useState(false);
+  const [status, setStatus] = useState(false);
 
-	const toggle = useCallback(() => {
-		const data = {
-			mode,
-			status: !status ? 'on' : 'off',
-		};
-		sendJsonMessage(data);
-		setStatus(!status);
-	}, [status, sendJsonMessage, mode]);
+  const toggle = useCallback(() => {
+    const data = {
+      mode,
+      status: !status ? "on" : "off",
+    };
+    sendJsonMessage(data);
+    setStatus(!status);
+  }, [status, sendJsonMessage, mode]);
 
-	const title = (mode) => {
-		switch (mode) {
-			case 'ignite':
-				return 'Ignition';
-			case 'eject':
-				return 'Ejection';
-			case 'eject2':
-				return 'Ejection2';
+  const title = (mode) => {
+    switch (mode) {
+      case "ignite":
+        return "Ignition";
+      case "eject":
+        return "Ejection";
+      case "reset":
+        return "Reset";
 
-			default:
-				break;
-		}
-	};
+      default:
+        break;
+    }
+  };
 
-	return (
-		<button
-			className="bg-transparent hover:bg-blue-500 text-blue-700 text-sm font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent disabled:opacity-50 disabled:cursor-not-allowed"
-			onClick={toggle}
-			disabled={readyState !== ReadyState.OPEN}
-		>
-			{status ? `Stop ${title(mode)}` : `Start ${title(mode)}`}
-		</button>
-	);
+  return (
+    <button
+      className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent"
+      onClick={toggle}
+      disabled={readyState !== ReadyState.OPEN}
+    >
+      {status ? `Stop ${title(mode)}` : `Start ${title(mode)}`}
+    </button>
+  );
 }
 
-export default Controls;
+function Reset({ mode, sendJsonMessage, readyState }) {
+  const reset = useCallback(() => {
+    const data = {
+      mode,
+      status: "on",
+    };
+    sendJsonMessage(data);
+  }, [sendJsonMessage, mode]);
+
+  return (
+    <button
+      className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent"
+      onClick={reset}
+      disabled={readyState !== ReadyState.OPEN}
+    >
+      Reset
+    </button>
+  );
+}
+
+export { Controls, Reset };
